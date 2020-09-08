c75bcc2917dd50db6459a9057837f2a665a98b35
za <m-zubairahmed@protonmail.com>
2020-09-08 17:03:33 +0530

    added config for entry and out put points for webpack and auto resolving for import from src and then from nodemodules

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,6 +1,15 @@
 const htmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+  mode: 'development',
+  entry: path.resolve('src', 'index.js'),
+  output: {
+    path: path.resolve('dist'),
+    filename: 'dist.bundle-[hash].js',
+    publicPath: '/',
+    pathinfo: true,
+  },
+  devtool: 'inline-source-map',
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' },
@@ -15,4 +24,7 @@ module.exports = {
       template: './src/index.html',
     }),
   ],
+  resolve: {
+    modules: [path.resolve('src'), path.resolve('node_modules')],
+  },
 }
