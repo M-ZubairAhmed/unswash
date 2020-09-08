e8e014b1090c395e7d4c5f69660ceac025daf1b4
za <m-zubairahmed@protonmail.com>
2020-09-08 17:32:21 +0530

    adjust browser compatibilty

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,14 @@ module.exports = function (api) {
     api.cache.using(() => process.env.NODE_ENV)
   }
 
-  const presets = ['@babel/preset-env', '@babel/preset-react']
+  const presets = [
+    '@babel/preset-env',
+    {
+      useBuiltIns: 'entry',
+      corejs: 3,
+    },
+    '@babel/preset-react',
+  ]
 
   const plugins = [
     '@babel/plugin-proposal-class-properties',
