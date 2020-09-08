7a73e507ed37cb97c81daaa1f0630f88d5ecd029
za <m-zubairahmed@protonmail.com>
2020-09-08 16:55:56 +0530

    added and configured html webpack plugin

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,5 +1,12 @@
+const htmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   module: {
     rules: [{ test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' }],
   },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
 }
