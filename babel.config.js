2a8f863d9b365768a7eb512c0c6cce424a6aaa4d
za <m-zubairahmed@protonmail.com>
2020-09-08 17:14:37 +0530

    added more babel plugins for more recent js features

--- babel.config.js
+++ babel.config.js
@@ -6,7 +6,14 @@ module.exports = function (api) {
 
   const presets = ['@babel/preset-env', '@babel/preset-react']
 
-  const plugins = []
+  const plugins = [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-export-namespace-from',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-proposal-throw-expressions',
+    '@babel/plugin-syntax-dynamic-import',
+  ]
 
   return {
     presets,
