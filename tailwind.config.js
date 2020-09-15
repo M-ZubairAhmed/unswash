297a4df2d85d1f159e63e0270adb5e120a84d2f8
za <m-zubairahmed@protonmail.com>
2020-09-15 15:51:15 +0530

    lint

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,9 @@ module.exports = {
   theme: {
     extend: {
       backgroundOpacity: {
-         '90': '0.90',
-      }
-    }
+        90: '0.90',
+      },
+    },
   },
   variants: {},
   plugins: [],
