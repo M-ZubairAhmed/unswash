6f728d50a8b2d0d5e8d1d988df48da9b21379e84
za <m-zubairahmed@protonmail.com>
2020-09-16 19:08:56 +0530

    lint

--- src/_pages/home.js
+++ src/_pages/home.js
@@ -743,7 +743,7 @@ const HomePage = () => {
     const locationWithSearchQuery = `${
       location.pathname
     }?${searchParams.toString()}`
-    
+
     history.push(locationWithSearchQuery)
   }
 
